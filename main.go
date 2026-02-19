6b305f24e400b4c7e93a86c71290183815822ae6
Harry Y. <harryyu02@gmail.com>
2026-02-18 19:01:54 -0500

    add register and reset command

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/harryyu02/gator/internal/config"
+	"github.com/harryyu02/gator/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -18,24 +26,6 @@ type command struct {
 
 type commands struct {
 	handlerMap map[string]func(*state, command) error
-}
-
-func printErrorAndExit(err error) {
-	fmt.Printf("err: %v\n", err)
-	os.Exit(1)
-}
-
-func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) == 0 {
-		return fmt.Errorf("command login expects a username\n")
-	}
-	user := cmd.args[0]
-	err := s.cfg.SetUser(user)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("User %s has been set\n", user)
-	return nil
 }
 
 func (c *commands) run(s *state, cmd command) error {
@@ -51,20 +41,88 @@ func (c *commands) register(name string, f func(*state, command) error) error {
 	return nil
 }
 
+func printErrorAndExit(err error) {
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func handleLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("command login expects a username\n")
+	}
+	username := cmd.args[0]
+
+	ctx := context.Background()
+	_, err := s.db.GetUser(ctx, username)
+	if err != nil {
+		return err
+	}
+
+	err = s.cfg.SetUser(username)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("User %s has been set\n", username)
+	return nil
+}
+
+func handleRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("command login expects a username\n")
+	}
+	username := cmd.args[0]
+
+	ctx := context.Background()
+	user, err := s.db.CreateUser(ctx, database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name: username,
+	})
+	if err != nil {
+		return err
+	}
+
+	handleLogin(s, command{
+		name: "login",
+		args: []string{username},
+	})
+
+	fmt.Println("User created!")
+	fmt.Printf("user: %v\n", user)
+
+	return nil
+}
+
+func handleReset(s *state, cmd command) error {
+	ctx := context.Background()
+	return s.db.DeleteAllUsers(ctx)
+}
+
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		printErrorAndExit(err)
 	}
+	db, err := sql.Open("postgres", cfg.DbUrl)
+	if err != nil {
+		printErrorAndExit(err)
+	}
+	dbQueries := database.New(db)
 	appState := state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 
-	handlerMap := make(map[string]func (*state, command) error)
+	handlerMap := make(map[string]func(*state, command) error)
 	appCommands := commands{
 		handlerMap: handlerMap,
 	}
-	appCommands.register("login", handlerLogin)
+	appCommands.register("login", handleLogin)
+	appCommands.register("register", handleRegister)
+	appCommands.register("reset", handleReset)
 
 	args := os.Args
 	if len(args) < 2 {
